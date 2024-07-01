fdead4af47e081487a7c34feaf64d8fb2b025ec7
Daniel Perdomo <136826084+GoblinSwarm@users.noreply.github.com>
2024-07-01 23:56:14 +0000

    Last changes

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 
-
 const initialTask = {
 	label: "",
 	is_done: false
@@ -21,49 +20,55 @@ const Home = () => {
 		})
 	}
 
+	const handleKeyDown = async (event) => {
+		if (event.key === "Enter") {
+			addTask(event);
+		}
+	}
+
 	const addTask = async (event) => {
 		try {
-			if (event.key === "Enter") {
-				if (task.label.trim() !== "") {
+			if (task.label.trim() !== "") {
+				const response = await fetch(`${URLBASE}/todos/Daniel_Perdomo`, {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify(task)
+				})
 
-					const response = await fetch(`${URLBASE}/todos/Daniel_Perdomo`, {
-						method: "POST",
-						headers: {
-							"Content-Type": "application/json"
-						},
-						body: JSON.stringify(task)
-					})
-
-					if (response.ok) {
-						getAllTask()
-						setTask(initialTask)
-					} else {
-						console.log("There is an error while adding the task.-")
-					}
+				if (response.ok) {
+					getAllTasks()
+					setTask(initialTask)
+				} else {
+					console.log("There was an error while adding the task.")
 				}
 			}
 		} catch (error) {
 			console.log(error)
 		}
-
 	}
 
-	const deleteTask = (id) => {
-		fetch(`${URLBASE}/todos/${id}`, {
-			method: "DELETE"
-		})
-			.then((response) => getAllTask())
-			.catch((error) => console.log(error))
+	const deleteTask = async (id) => {
+		try {
+			const response = await fetch(`${URLBASE}/todos/${id}`, {
+				method: "DELETE"
+			})
+			if (response.ok) {
+				getAllTasks()
+			}
+		} catch (error) {
+			console.log(error)
+		}
 	}
 
-	const getAllTask = async () => {
+	const getAllTasks = async () => {
 		try {
 			let response = await fetch(`${URLBASE}/users/Daniel_Perdomo`)
 			let data = await response.json()
 
-			if (response.status == 404) {
+			if (response.status === 404) {
 				createUser()
-				getAllTask()
 			} else {
 				setTodos(data.todos)
 			}
@@ -76,31 +81,30 @@ const Home = () => {
 		try {
 			let response = await fetch(`${URLBASE}/users/Daniel_Perdomo`, {
 				method: "POST"
-
 			})
-
-			console.log(response)
+			if (response.ok) {
+				getAllTasks()
+			}
 		} catch (error) {
 			console.log(error)
 		}
 	}
 
-	async function deleteAll() {
+	const deleteAll = async () => {
 		try {
 			let response = await fetch(`${URLBASE}/users/Daniel_Perdomo`, {
 				method: "DELETE"
 			})
-			if (response.status == 204) {
-				getAllTask()
+			if (response.status === 204) {
+				getAllTasks()
 			}
-
 		} catch (error) {
 			console.log(error)
 		}
 	}
 
 	useEffect(() => {
-		getAllTask()
+		getAllTasks()
 	}, [])
 
 	return (
@@ -108,9 +112,7 @@ const Home = () => {
 			<div className="row justify-content-center">
 				<div className="col-12 col-md-7">
 					<h1 className="my-3">Todo list</h1>
-					<form
-						onSubmit={(event) => event.preventDefault()}
-					>
+					<form onSubmit={(event) => event.preventDefault()}>
 						<input
 							className="form-control"
 							type="text"
@@ -118,29 +120,28 @@ const Home = () => {
 							name="label"
 							value={task.label}
 							onChange={handleChange}
-							onKeyDown={addTask}
+							onKeyDown={handleKeyDown}
 						/>
 					</form>
 					<br />
 					{
 						todos.map((item) => {
 							return (
-								<div key={item.id}
-									className="d-flex justify-content-between"
-								>
-									<div>
-										{item.label}
-									</div>
+								
+								<div key={item.id} className="d-flex justify-content-between">
+									<div>{item.label}</div>
+									<br />
 									<button
 										className="btn btn-danger"
 										onClick={() => deleteTask(item.id)}
-									>x</button>
+									><i class="fas fa-times"></i></button>
 								</div>
 							)
 						})
 					}
 					<button
-						onClick={() => { deleteAll() }}
+						className="btn btn-danger mt-3"
+						onClick={deleteAll}
 					>Eliminar todo</button>
 				</div>
 			</div>
